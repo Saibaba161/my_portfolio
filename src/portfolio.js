875d0194209c1d8719994fe8120682382c8d3684
Saibaba-1610 <sai1610k@gmail.com>
2024-04-23 22:48:00 +0530

    Updated Logo

--- src/portfolio.js
+++ src/portfolio.js
@@ -254,7 +254,7 @@ const publications = {
 const contactPageData = {
   contactSection: {
     title: "Contact Me",
-    profile_image_path: "animated_ashutosh.png",
+    profile_image_path: "sai_animated.png",
     description:
       "I am available on almost every social media. You can message me, I will reply within 24 hours.",
   },
